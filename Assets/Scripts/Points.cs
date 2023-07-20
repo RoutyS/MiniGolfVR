b8c3925fc8ecc4f7f3caa073190ae9502affea18
soetshi <90394673+soetshi@users.noreply.github.com>
2023-07-20 17:45:02 +0200

    Détection de collision dans le trou

--- Assets/Scripts/Points.cs
+++ Assets/Scripts/Points.cs
@@ -1,10 +1,11 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Points : MonoBehaviour
 {
-  
+    public GameObject cube;
     void Start()
     {
         
@@ -14,5 +15,13 @@ public class Points : MonoBehaviour
     void Update()
     {
         
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Trou")
+        {
+            cube.SetActive(true);
+        }
     }
 }
