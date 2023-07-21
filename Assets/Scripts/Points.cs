4b0ce4e9888ee81cef00b0601cbacf0d5b1da984
soetshi <90394673+soetshi@users.noreply.github.com>
2023-07-21 09:00:26 +0200

    prep pour merge

--- Assets/Scripts/Points.cs
+++ Assets/Scripts/Points.cs
@@ -6,25 +6,25 @@ using System.Collections;
 using System.Collections.Generic;
 
 using UnityEngine.UI;
+
 public class Points : MonoBehaviour
 {
     public GameObject cube;
-    
     public Text txt;
     public int score = 0;
+    private bool canScore = true;
 
     void Start()
     {
-        txt.text = score.ToString(); 
+        txt.text = score.ToString();
     }
 
-   
     void Update()
     {
         if (score > 6)
         {
             score = 0;
-            gameObject.SetActive(false); 
+            gameObject.SetActive(false);
         }
     }
 
@@ -33,15 +33,27 @@ public class Points : MonoBehaviour
         if (collision.gameObject.tag == "Trou")
         {
             cube.SetActive(false);
-            score=0;
+            score = 0;
             txt.text = score.ToString();
             GameManager.Instance.AddScore(score);
-           
         }
-        if (collision.gameObject.tag == "bout")
+
+        if (collision.gameObject.tag == "bout" && canScore)
         {
-            score++;
-            txt.text = score.ToString();
+            ScoreIncrement();
         }
+    }
+
+    void ScoreIncrement()
+    {
+        score++;
+        txt.text = score.ToString();
+        canScore = false;
+        Invoke("EnableScoring", 5f); // après 5 secondes, EnableScoring() sera appelé
+    }
+
+    void EnableScoring()
+    {
+        canScore = true;
     }
 }
